11501eb112d9660cd67d681c543052888dc77840
XindaQ <60855181+XindaQ@users.noreply.github.com>
2022-11-03 12:45:22 -0400

    Update layer_utils.py

--- model/layer_utils.py
+++ model/layer_utils.py
@@ -4,7 +4,7 @@ import torch
 import torch.nn as nn
 from torchvision import models
 
-CONV3_3_IN_VGG_19 = models.vgg19(pretrained=True).features[:15].cuda()
+CONV3_3_IN_VGG_19 = models.vgg19(pretrained=True).features[:15].cuda()              # bring a pretrained model
 
 
 def get_norm_layer(norm_type='instance'):
@@ -39,7 +39,7 @@ class ResNetBlock(nn.Module):
         padding = self._chose_padding_type(padding_type, sequence)
 
         sequence += [
-            nn.Conv2d(dim, dim, kernel_size=3, stride=1, padding=padding, bias=use_bias),
+            nn.Conv2d(dim, dim, kernel_size=3, stride=1, padding=padding, bias=use_bias),     # keep the image in same channel, size will decrease by 2 using k=3
             norm_layer(dim),
             nn.ReLU(True)
         ]
@@ -49,19 +49,19 @@ class ResNetBlock(nn.Module):
 
         self.model = nn.Sequential(*sequence)
 
-    def _chose_padding_type(self, padding_type, sequence):
+    def _chose_padding_type(self, padding_type, sequence):                    # different padding types
         padding = 0
         if padding_type == 'reflect':
-            sequence += [nn.ReflectionPad2d(1)]
+            sequence += [nn.ReflectionPad2d(1)]                               # padding using the reflextion of the bound  (+1 for each side)                             
         elif padding_type == 'replicate':
-            sequence += [nn.ReplicationPad2d(1)]
+            sequence += [nn.ReplicationPad2d(1)]                              # padding using the relication
         elif padding_type == 'zero':
-            padding = 1
+            padding = 1                                                       # padding by using "1"
         else:
             raise NotImplementedError('padding [%s] is not implemented' % padding_type)
         return padding
 
-    def forward(self, x):
+    def forward(self, x):                                                               # use resnet idea in the forward calculation
         out = x + self.model(x)
         return out
 
@@ -75,22 +75,22 @@ class MinibatchDiscrimination(nn.Module):
         self.out_features = out_features  # B
         self.kernel_dims = kernel_dims  # C
         self.mean = mean
-        self.T = nn.Parameter(torch.Tensor(in_features, out_features, kernel_dims)).cuda()  # AxBxC
-        nn.init.normal_(self.T, 0, 1)
+        self.T = nn.Parameter(torch.Tensor(in_features, out_features, kernel_dims)).cuda()  # AxBxC, this is the weights
+        nn.init.normal_(self.T, 0, 1)            # initial weights in normal distribution (0,1)
 
     def forward(self, x):
         # x is NxA
         # T is AxBxC
-        matrices = x.mm(self.T.view(self.in_features, -1))  # NxBC
-        matrices = matrices.view(-1, self.out_features, self.kernel_dims)  # NxBxC
+        matrices = x.mm(self.T.view(self.in_features, -1))  # NxBC  <- NxA * AxBC
+        matrices = matrices.view(-1, self.out_features, self.kernel_dims)  # NxBxC  <- NxBC
 
-        M = matrices.unsqueeze(0)  # 1xNxBxC
-        M_T = M.permute(1, 0, 2, 3)  # Nx1xBxC
-        norm = torch.abs(M - M_T).sum(3)  # NxNxB
+        M = matrices.unsqueeze(0)  # 1xNxBxC  <- NxBxC          # add a new dimension in front, if -1, then add at back
+        M_T = M.permute(1, 0, 2, 3)  # Nx1xBxC  <- 1xNxBxC
+        norm = torch.abs(M - M_T).sum(3)  # NxNxB               # boardcast to NxN and then sum across the kernel_dim
         expnorm = torch.exp(-norm)
-        o_b = (expnorm.sum(0))  # NxB
+        o_b = (expnorm.sum(0))  # NxB                           # sum across the Batch size
         if self.mean:
-            o_b /= x.size(0)
+            o_b /= x.size(0)                                    # get the average for each sample if mean
 
-        x = torch.cat((x, o_b), 1)  # Nx(A+B)
+        x = torch.cat((x, o_b), 1)  # Nx(A+B)                   # get the input and its converted features for each sample in the batch
         return x
