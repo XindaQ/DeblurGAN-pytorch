6c0e3ba28a9b827ae41c908782b50e033e84657c
XindaQ <60855181+XindaQ@users.noreply.github.com>
2022-11-12 16:29:04 -0500

    Update model.py

--- model/model.py
+++ model/model.py
@@ -68,13 +68,80 @@ class ResNetGenerator(BaseModel):
                                                                                 # or also use this with a proper normalization
             out = torch.clamp(out, min=-1, max=1)  # clamp to [-1,1] according to normalization(mean=0.5, var=0.5)      # clamp the output for a proper data 
         return out                                                              # mean=0.5, var=0.5 means -0.5 and then /0.5, to make data in range [-1, 1] 
+    
+class ResNetGeneratorReduced(BaseModel):
+    """
+    Define a generator using ResNet
+    
+    Less channels and layers: 10 -> 10 -> 5 -> 5x 5 -> 10 -> 10, with a skip connection
+    """
+
+    def __init__(self, input_nc, output_nc, ngf=16, n_blocks=5, norm_type='instance', padding_type='reflect',
+                 use_dropout=True, learn_residual=True):
+        super(ResNetGenerator, self).__init__()
+                                                                              # ngf: number of filters in generator (channels)
+        self.learn_residual = learn_residual
+
+        norm_layer = get_norm_layer(norm_type)                                # get the proper norm layer
+        if type(norm_layer) == functools.partial:  # no need to use bias as BatchNorm2d has affine parameters
+            use_bias = norm_layer.func != nn.BatchNorm2d
+        else:
+            use_bias = norm_layer != nn.BatchNorm2d         # turn off the bias if BatchNorm2d is used
+            
+                                                                                # the first conv layer to expand the channels (1/3 -> ngf)
+        sequence = [                                        # padding(x+6) -> conv2d (7, 1 -> x-6) -> instance norm (apply to each image(H*W)) -> ReLU
+            nn.ReflectionPad2d(3),                          # the size of image not change (x -> x)
+            nn.Conv2d(input_nc, ngf, kernel_size=7, stride=1, padding=0, bias=use_bias),
+            norm_layer(ngf),                                # point out the channels in (N*C*H*W)
+            nn.ReLU(True)                                   # use the ReLU as activation function
+        ]
+
+        n_downsampling = 1
+        for i in range(n_downsampling):  # downsample the feature map           # front part of the U-net
+            mult = 2 ** i
+            sequence += [
+                nn.Conv2d(ngf * mult, ngf * mult * 2, kernel_size=3, stride=2, padding=1, bias=use_bias),           # double the channel and half the size (x -> x/2)
+                norm_layer(ngf * mult * 2),                                                                         # norm layer
+                nn.ReLU(True)                                                           # do the relu calculation in place to save some memory                                                                   
+            ]
+
+        for i in range(n_blocks):  # ResNet                                     # stack of multiple resnets, keep size and channel still
+            sequence += [
+                ResNetBlock(ngf * 2 ** n_downsampling, norm_layer, padding_type, use_dropout, use_bias)
+            ]
+
+        for i in range(n_downsampling):  # upsample the feature map             # back part of the U-net, upsampling, make it has same layer with as front part
+            mult = 2 ** (n_downsampling - i)                                    # calculate the channels
+            sequence += [
+                nn.ConvTranspose2d(ngf * mult, int(ngf * mult / 2), kernel_size=3, stride=2, padding=1,         # half the channel and double the size (x -> 2x)
+                                   output_padding=1, bias=use_bias),
+                norm_layer(int(ngf * mult / 2)),
+                nn.ReLU(True)
+            ]
+
+        sequence += [                                                           # the final conv layer to map the channels to output, size donot change (x -> x)
+            nn.ReflectionPad2d(3),
+            nn.Conv2d(ngf, output_nc, kernel_size=7, stride=1, padding=0),
+            nn.Tanh()                                                           # use the tanh as activation function to control the output range (-1, 1)
+        ]                                                                       
+
+        self.model = nn.Sequential(*sequence)                                   # get the whole model
+
+    def forward(self, x):
+        out = self.model(x)                                                     # get the output
+        if self.learn_residual:                                                 # put a high layer skip connection at higest level
+            out = x + out                                                       # use output = x + x_range * out to adjust the proper range
+                                                                                # or also use this with a proper normalization
+            out = torch.clamp(out, min=-1, max=1)  # clamp to [-1,1] according to normalization(mean=0.5, var=0.5)      # clamp the output for a proper data 
+        return out  
+            
 
 
 class NLayerDiscriminator(BaseModel):
     """Define a PatchGAN discriminator"""
 
     def __init__(self, input_nc, ndf=64, n_layers=3, norm_type='instance', use_sigmoid=False,               # try to get the representation for images
-                 use_minibatch_discrimination=False):                                           # 360*640 -> 180 -> 90/45/22 -> 21 -> 20
+                 use_minibatch_discrimination=False):                                           # 360(*640) -> 180 -> 90/45/22 -> 21 -> 20
         super(NLayerDiscriminator, self).__init__()                                   # finally use a matrix (1-channel feature tensor) as the classification result
 
         self.use_minibatch_discrimination = use_minibatch_discrimination
